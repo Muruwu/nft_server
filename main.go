d80f5610a44d6c384fb9ef80213cd8d51dae1c8c
wuqi.murux <wuqi.murux@bytedance.com>
2022-07-12 20:44:42 +0800

    feat: accomplish idl interface

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/server"
+	"github.com/mc_nft/middleware"
 	"log"
 
 	mc_nft "github.com/mc_nft/kitex_gen/mc_nft/nftservice"
 )
 
 func main() {
-	svr := mc_nft.NewServer(new(NftServiceImpl))
+	svr := mc_nft.NewServer(new(NftServiceImpl),
+		server.WithMiddleware(middleware.RequestValidateMW))
 
 	err := svr.Run()
 
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # start
-> 使用 https://github.com/cloudwego/kitex 框架
+## Overview
+* gRPC protocol 
+* serialization protocol: protobuf
+* lang: go
 
+
+## 技术选型
+* 服务框架
+使用 https://github.com/cloudwego/kitex 框架
+* 数据库使用gorm
+  https://github.com/go-gorm/gorm
+* gorm model code generate 使用
+  https://github.com/go-gorm/gen
